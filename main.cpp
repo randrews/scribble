d7db6bef6c30dabe8e03368d23532445e1cf4c0d
Ross Andrews <ross.andrews@gmail.com>
2017-09-02 23:14:25 -0500

    Mouse click events

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ int handleEvent(SDL_Event*, SDL_Renderer*);
 Uint32 timerCallback(Uint32 interval, void *param);
 void redraw(SDL_Renderer*);
 void resize(SDL_Renderer *ren, SDL_Window *win);
+const char* mouse_button_name(int button);
 
 Array primitives, effects, textures;
 
@@ -69,15 +70,38 @@ void resize(SDL_Renderer *ren, SDL_Window *win) {
 }
 
 int handleEvent(SDL_Event *event, SDL_Renderer *ren) {
+    const char *btn;
+
     switch(event->type){
     case SDL_WINDOWEVENT:
         if(event->window.event == SDL_WINDOWEVENT_CLOSE) return 1;
         break;
     case SDL_QUIT: return 1; break;
-    case SDL_USEREVENT: return 2;
+    case SDL_USEREVENT: return 2; break;
+    case SDL_MOUSEBUTTONDOWN:
+        SDL_LockMutex(output_mutex);
+        printf("mousedown %s %d %d\n", mouse_button_name(event->button.button), event->button.x, event->button.y);
+        SDL_UnlockMutex(output_mutex);
+        break;
+    case SDL_MOUSEBUTTONUP:
+        SDL_LockMutex(output_mutex);
+        printf("mouseup %s %d %d\n", mouse_button_name(event->button.button), event->button.x, event->button.y);
+        SDL_UnlockMutex(output_mutex);
+        break;
     }
 
     return 0;
+}
+
+const char* mouse_button_name(int button) {
+    switch(button) {
+    case SDL_BUTTON_LEFT: return "left";
+    case SDL_BUTTON_RIGHT: return "right";
+    case SDL_BUTTON_MIDDLE: return "middle";
+    case SDL_BUTTON_X1: return "x1";
+    case SDL_BUTTON_X2: return "x2";
+    default: return "?";
+    }
 }
 
 void redraw(SDL_Renderer *ren) {
